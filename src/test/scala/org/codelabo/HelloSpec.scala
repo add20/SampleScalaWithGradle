50f6e8d4cb768b1039a0fbbc672569a5af7cf002
add20 <add20g@gmail.com>
2015-07-12 10:48:54 +0900

    Bump up scala version 2.10.0 -> 2.11.7

--- src/test/scala/org/codelabo/HelloSpec.scala
+++ src/test/scala/org/codelabo/HelloSpec.scala
@@ -3,10 +3,10 @@ package org.codelabo
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSpec
-import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Matchers
 
 @RunWith(classOf[JUnitRunner])
-class HelloSpec extends FunSpec with ShouldMatchers {
+class HelloSpec extends FunSpec with Matchers {
   describe("Hello.hello") {
     it("shuld return string \"hello\"") {
       new Hello().hello should equal("hello")
